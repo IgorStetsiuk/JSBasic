1d02eed9241d48bde5e63932b257c3dc492a8b1a
Igor Stetsiuk <igorstetsuk@gmail.com>
2018-01-03 13:24:19 +0200

    edit

---
+++ campTasks/task_1_arrays/description.js
@@ -0,0 +1,29 @@
+/*
+
+    1. Remove duplicates.
+        Write a function that removes duplicate values from an array.
+        Example:
+
+        removeDuplicates([1, 3, 7, 1, 3, 9, 8, 7]);     //  [1, 3, 7, 9, 8]
+
+    2. Common values.
+        Write a function that returns the common values of two arrays.
+        Example:
+
+        commonValues([3, 4, 6, 3, 1], [5, 10, 7, 1, 3, 9, 8, 7]);     //  [3, 1]
+
+    3. Distinct values.
+        Write a function that returns the distinct values of two arrays.
+        Example:
+
+        distinctValues([3, 4, 6, 3, 1], [5, 10, 7, 1, 3, 9, 8, 7]);     //  [4, 6, 5, 10, 7, 9, 8]
+
+    4. Random.
+        Write a function which creates an array with defined size and fills it with random values
+
+        function generateArr(arrSize) {...};
+
+    5. Includes.
+        Write your own implementation of Array.prototype.includes() method.
+
+ */
---
+++ campTasks/task_1_arrays/solutions.js
@@ -0,0 +1,9 @@
+/*
+1. Remove duplicates.
+    Write a function that removes duplicate values from an array.
+    Example:
+
+removeDuplicates([1, 3, 7, 1, 3, 9, 8, 7]);     //  [1, 3, 7, 9, 8]
+*/
+
+
