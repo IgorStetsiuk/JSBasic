b136f4ca6ab72e9cdad3797e85f98013e37219ca
Igor Stetsiuk <igorstetsuk@gmail.com>
2018-01-03 18:17:13 +0200

    first task

--- campTasks/task_1_arrays/solutions.js
+++ campTasks/task_1_arrays/solutions.js
@@ -7,3 +7,14 @@ removeDuplicates([1, 3, 7, 1, 3, 9, 8, 7]);     //  [1, 3, 7, 9, 8]
 */
 
 
+function removeDuplicates(array) {
+    let result = [];
+    for (let i = 0, length = array.length; i < length; i++) {
+        if (result.indexOf(array[i]) === -1) result.push(array[i]);
+    }
+    return result
+
+}
+
+console.log(removeDuplicates([1, 3, 7, 1, 3, 9, 8, 7]));
+console.log(removeDuplicates([1, 1, 1, 1]));
