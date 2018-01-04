4b8f4303b90fccdfb30843c277d982c00d72fba8
IgorStetsiuk <igorstetsuk@gmail.com>
2018-01-05 00:12:30 +0200

    second task done

--- campTasks/task_1_arrays/solutions.js
+++ campTasks/task_1_arrays/solutions.js
@@ -16,5 +16,38 @@ function removeDuplicates(array) {
 
 }
 
-console.log(removeDuplicates([1, 3, 7, 1, 3, 9, 8, 7]));
-console.log(removeDuplicates([1, 1, 1, 1]));
+console.log(removeDuplicates([1, 3, 7, 1, 3, 9, 8, 7]), 'remove duplicates test_1');
+console.log(removeDuplicates([1, 1, 1, 1]), 'remove duplicates test_2');
+
+
+/*
+2. Common values.
+    Write a function that returns the common values of two arrays.
+    Example:
+
+commonValues([3, 4, 6, 3, 1], [5, 10, 7, 1, 3, 9, 8, 7]);     //  [3, 1]*/
+
+function commonValues(arr1, arr2) {
+    let resultedArray = [];
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            if (arr1[i] === arr2[j] && resultedArray.indexOf(arr1[i]) === -1) {
+                resultedArray.push(arr1[i]);
+            }
+        }
+    }
+    return resultedArray;
+}
+
+console.log(commonValues([3, 4, 6, 3, 1], [5, 10, 7, 1, 3, 9, 8, 7]), 'common values');
+
+function commonValues(arr1, arr2) {
+    return arr1.reduce((prev, next) => {
+        if (arr2.includes(next) && !prev.includes(next)) {
+            return [...prev, next];
+        }
+        return prev;
+    }, [])
+}
+
+console.log(commonValues([3, 4, 6, 3, 1, 11, 1, 0], [0, 5, 10, 7, 1, 3, 9, 8, 7, 0]), 'common values - solution 2');
